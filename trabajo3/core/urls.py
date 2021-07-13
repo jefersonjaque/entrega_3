2366d535603fe176422c7e44c5f6fee5055d362c
jefersonjaque <jefersonjaque12@gmail.com>
2021-07-13 18:19:08 -0400

    actualizacion

--- trabajo3/core/urls.py
+++ trabajo3/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home,cajas
 
 urlpatterns = [
-    path('',home,name="home")
+    path('',home,name="home"),
+    path('cajas/',cajas,name="cajas")
 ]
--- trabajo3/core/views.py
+++ trabajo3/core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'core/index.html')
+
+def cajas(request):
+    return render(request,'core/cajas.html')
